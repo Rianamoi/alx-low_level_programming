9e5d2cf728bf0707f28a4726481021595b869fed
Riana <rianamoi@gmail.com>
2022-10-03 13:03:42 -0700

    use malloc function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ *  and initializes it with a specific char.
+ * @size: size of the array
+ * @c: char to initialize
+ * Return: pointer to the array initialized or NULL
+ */
+
+
+
+char *create_array(unsigned int size, char c)
+
+{
+
+	char *array = malloc(size);
+
+		if (size == 0 || array == 0)
+
+			return (0);
+
+		while (size--)
+
+			array[size] = c;
+
+	return (array);
+}
