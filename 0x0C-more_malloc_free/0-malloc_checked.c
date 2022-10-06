b01528b33e61a0a7ea9147b9f494abb9574c9843
Riana <rianamoi@gmail.com>
2022-10-06 07:52:09 -0700

    allocate memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *m = malloc(b);
+
+	if (m == NULL)
+		exit(98);
+
+	return (m);
+}
