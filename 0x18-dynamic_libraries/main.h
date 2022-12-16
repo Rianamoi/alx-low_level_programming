d58963af8a9b91b21967bcd1dae602fb967aeacd
Riana <rianamoi@gmail.com>
2022-12-16 05:49:07 -0800

    make operations

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -44,6 +44,14 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
+int add(int a, int b);
 
+int sub(int a, int b);
+
+int mod(int a, int b);
+
+int mul(int a, int b);
+
+int div(int a, int b);
 
 #endif
