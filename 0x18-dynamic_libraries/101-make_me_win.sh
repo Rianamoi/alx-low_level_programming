675983581d731444c3f7a67855dd0f6a7ea1cb53
Rianamoi <111004345+Rianamoi@users.noreply.github.com>
2022-12-16 17:29:39 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/make_me_win.so
+wget -P /tmp https://github.com/Rianamoi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/make_me_win.so
 export LD_PRELOAD=/tmp/make_me_win.so
